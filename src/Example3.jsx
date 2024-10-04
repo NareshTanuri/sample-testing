2a01cc74ee16e0341b258cbc45ba1d9c14b3ad02
Naresh Tanuri <nareshtanuri8@gmail.com>
2024-10-04 17:15:42 +0530

    update the example file with commenting whole file

--- src/Example3.jsx
+++ src/Example3.jsx
@@ -1,41 +1,41 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+// import axios from "axios";
+// import React, { useEffect, useState } from "react";
 
-const Example3 = () => {
-    const [data, setData] = useState([]);
-    const [displayedData, setDisplayedData] = useState([]);
-    const [currentBatch, setCurrentBatch] = useState(0);
+// const Example3 = () => {
+//     const [data, setData] = useState([]);
+//     const [displayedData, setDisplayedData] = useState([]);
+//     const [currentBatch, setCurrentBatch] = useState(0);
     
-    useEffect(() => {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(response => {
-                setData(response.data);
-            })
-            .catch(error => {
-                console.error("Failure", error);
-            });
-    }, []);
+//     useEffect(() => {
+//         axios.get('https://jsonplaceholder.typicode.com/posts')
+//             .then(response => {
+//                 setData(response.data);
+//             })
+//             .catch(error => {
+//                 console.error("Failure", error);
+//             });
+//     }, []);
 
-    useEffect(() => {
-        if (data.length > 0 && currentBatch * 20 < data.length) {
-            const timeout = setTimeout(() => {
-                setDisplayedData(prevDisplayedData => [
-                    ...prevDisplayedData,
-                    ...data.slice(currentBatch * 20, (currentBatch + 1) * 20)
-                ]);
-                setCurrentBatch(prevBatch => prevBatch + 1);
-            }, 9000);
-            return () => clearTimeout(timeout);
-        }
-    }, [data, currentBatch]);
+//     // useEffect(() => {
+//     //     if (data.length > 0 && currentBatch * 20 < data.length) {
+//     //         const timeout = setTimeout(() => {
+//     //             setDisplayedData(prevDisplayedData => [
+//     //                 ...prevDisplayedData,
+//     //                 ...data.slice(currentBatch * 20, (currentBatch + 1) * 20)
+//     //             ]);
+//     //             setCurrentBatch(prevBatch => prevBatch + 1);
+//     //         }, 9000);
+//     //         return () => clearTimeout(timeout);
+//     //     }
+//     // }, [data, currentBatch]);
 
-    return (
-        <>
-            {displayedData.map((item, index) => (
-                <div key={index}>{index + 1}.{item.title}</div>
-            ))}
-        </>
-    );
-};
+//     return (
+//         <>
+//             {displayedData.map((item, index) => (
+//                 <div key={index}>{index + 1}.{item.title}</div>
+//             ))}
+//         </>
+//     );
+// };
 
-export default Example3;
+// export default Example3;
